4b34972de1e615721ce807b674b170a954d41aa4
Chayanne Lazcano G <chayanne_97@hotmail.com>
2022-06-07 20:45:12 -0500

    primer commit

---
+++ excercises-array/main.js
@@ -0,0 +1,17 @@
+// arrays 
+
+// coleccion ordenada de datos, se emplea para almacenar multiples valores en una sola variable
+
+//sintaxis 
+//var nombredelArray =[ ];
+ 
+//Acceder a los valores del array
+// nombredelArray [posicion]
+
+//ejemplo
+var colores = ['negro','verde','rojo','azul','naranja' ];
+
+//console.log(colores);
+
+colores['negro'] = "gris"
+console.log(colores);
