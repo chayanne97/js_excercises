ae5128d12d6605ef49e9ac2fda7f1708f05f78cc
Chayanne Lazcano G <chayanne_97@hotmail.com>
2022-06-08 20:26:25 -0500

    semaforo inicio

--- Funciones/main.js
+++ Funciones/main.js
@@ -4,14 +4,18 @@
 // 3er dentro de {} poner codigo a ejecutar por mi funcion 
 // ejecutar nuestra funcion 
 
-function resta (a,b){
-    var resta=parseint(a)-parseint(b);
-    return 'la resta es: '+ resta; 
-}
+//function resta (a,b){
+  //  var resta=parseint(a)-parseint(b);
+    //return 'la resta es: '+ resta; 
+//}
 //solicitar dos numeros al usuario 
-var inputusuario1=prompt('Ingrese un numero')
-var inputusuario2=prompt('ingrese otro numeor')
+//var inputusuario1=prompt('Ingrese un numero')
+//var inputusuario2=prompt('ingrese otro numero')
 //guardamos el resultado en una variable
-var resultado= resta(inputusuario1,inputusuario2)
+//var resultado= resta(inputusuario1,inputusuario2)
 
-console.log(resultado)
+//console.log(resultado)
+
+// funciones anidadas; fundion dentro de otr funcion
+
+function ObtenerMarcador ()
