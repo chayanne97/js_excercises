416f8ea0c29af256a1f8b7489923357bdfd62e37
Chayanne Lazcano G <chayanne_97@hotmail.com>
2022-06-13 18:51:40 -0500

    primer ejercicio de estructuras de control

--- Excercises-init/main.js
+++ Excercises-init/main.js
@@ -1,3 +1,9 @@
-console.log("hola",prompt("Dime tu nombre: "));
-
-
+//preguntar al usuario si es bellisimo 
+var pregunta=prompt("¿Eres bellisimo?")
+if (pregunta=="si"||pregunta=="sí"||pregunta=="SI"||pregunta=="SÍ"){
+    alert("Muy cierto, eres encantador@")
+}else if (pregunta=="no"||pregunta=="NO"||pregunta=="No"){
+    alert("no te creo, piensalo bien")
+}else{ 
+    alert ("no te entiendo")
+}
