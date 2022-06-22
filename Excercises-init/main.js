cda13a5c47b3daf12196875913f7b9c50a830cd1
Chayanne Lazcano G <chayanne_97@hotmail.com>
2022-06-22 18:42:00 -0500

    ejercicio condicional 2

--- Excercises-init/main.js
+++ Excercises-init/main.js
@@ -7,3 +7,10 @@ if (pregunta=="si"||pregunta=="sí"||pregunta=="SI"||pregunta=="SÍ"){
 }else{ 
     alert ("no te entiendo")
 }
+// preguntar al usuario por un numero y deternminar si es divisible entre dos
+let numberUser=prompt("ingrese un numero por favor")
+if (numberUser % 2 == 0){
+    alert(numberUser + " es divisible entre dos")
+}else{
+    alert(numberUser + " no es divisible entre dos")
+}
