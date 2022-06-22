0dd2534517ef11264d9d4839245978856e9054a0
Chayanne Lazcano G <chayanne_97@hotmail.com>
2022-06-22 11:20:19 -0500

    ehercicio1

---
+++ Ejercicios-Es6/main.js
@@ -0,0 +1,15 @@
+const animalitos=[
+    {nombre:"Carlitos", especie:"conejo"},//0
+    {nombre:"Esteban", especie:"perro"},//1
+    {nombre:"Fabiruchis", especie:"tortuga"},//2
+    {nombre:"anita", especie:"gato"},//3
+    {nombre:"miranda", especie:"conejo"},//4
+    {nombre:"lucas", especie:"conejo"},//5
+    {nombre:"Horacia", especie:"tortuga"},//6
+]
+function soloconejos(conejo){
+    return soloconejos.conejo
+}
+
+console.log(soloconejos)
+
